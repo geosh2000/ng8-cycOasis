87f49ea7cc6b0906c716124570618a14b6cab667
Jorge Sanchez <geosh2000@gmail.com>
2020-09-22 16:34:23 -0500

    Go To Loc by Confirmation

--- src/app/shared/go-to-loc/go-to-loc.component.ts
+++ src/app/shared/go-to-loc/go-to-loc.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from 'src/app/services/service.index';
 
 @Component({
   selector: 'app-go-to-loc',
@@ -9,16 +11,52 @@ import { Router } from '@angular/router';
 export class GoToLocComponent implements OnInit {
 
   value:any
+  loading:Object = {}
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, private _api: ApiService, private toastr:ToastrService) { }
 
   ngOnInit() {
   }
 
-  goTo( v ){
+  goTo( v:String ){
+
     this.router.navigate(['/rsv2',v]);
     // tslint:disable-next-line: no-unused-expression
     this.value = ''
+  }
+
+  searchConf( v:String ){
+
+    if( v.trim().length >= 8 ){
+
+      this.loading['searchLoc'] = true
+
+      this._api.restfulGet( v.trim(), 'Rsv/searchMlByConf' )
+                  .subscribe( res => {
+
+                    this.loading['searchLoc'] = false;
+
+                    if( !res['data'] ){
+                      this.toastr.error( `La confirmación ${ v.trim() }, no está ligada a ningún MasterLocator`, 'Inexistente' );
+                      this.value = ''
+                    }else{
+                      this.goTo( res['data'] )
+                    }
+                    
+
+                  }, err => {
+                    this.loading['searchLoc'] = false;
+
+                    const error = err.error;
+                    this.toastr.error( error.msg, err.status );
+                    console.error(err.statusText, error.msg);
+
+                  });
+
+    }else{
+      this.goTo( v.trim() )
+    }
+
   }
 
 }
